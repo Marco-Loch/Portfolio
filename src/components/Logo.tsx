ac6e0d2bdb71026d9ecb493b346778034b6ebe1e
Marco Loch <cn_macro@web.de>
2025-09-03 01:04:53 +0200

    logo type ausgelagert

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,7 @@
 import type { FC } from 'react';
 import { Box } from '@mui/material';
 import LogoImage from '../assets/img/faviconML.png';
-
-interface LogoProps {
-  width?: number | string;
-  height?: number | string;
-}
+import type { LogoProps } from '../types/logo_type';
 
 const Logo: FC<LogoProps> = ({ width = 80, height = 80 }) => {
   return <Box component="img" src={LogoImage} alt="Marco Loch Logo" sx={{ width, height, mb: 1 }} />;
